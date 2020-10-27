eac909145342d2829de5810c55c3c2a948d74118
Cynthia Navarro <cynavarro@MacBook-Pro-6.local>
2020-10-27 10:34:11 -0700

    adding new updates to app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,7 +1,22 @@
 import React from "react"
-import PropTypes from "prop-types"
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './pages/Home'
+import ApartmentIndex from './pages/ApartmentIndex'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
 
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apartments: []
+    }
+  }
+
   render () {
     const {
       logged_in,
@@ -9,19 +24,44 @@ class App extends React.Component {
       sign_out_route,
     } = this.props
     return (
-      <React.Fragment>
-        <h1>The Apartment App</h1>
-        { logged_in &&
-          <div>
-            <a href={ sign_out_route }>Sign Out</a>
-          </div>
-        }
-        { !logged_in &&
-          <div>
-            <a href={sign_in_route}>Sign In</a>
-          </div>
-        }
-      </React.Fragment>
+      <Router>   
+
+        <Header
+          logged_in={ this.props.logged_in }
+          sign_in_route={ this.props.sign_in_route }
+          sign_up_route={ this.props.sign_up_route }
+          sign_out_route={ this.props.sign_out_route }
+        />
+        <Switch>
+          <Route
+            exact path="/"
+            render={ (props) =>
+              <Home
+                logged_in={ logged_in }
+                sign_in_route={ sign_in_route }
+                sign_out_route={ sign_out_route }
+              />
+            }
+          />
+          <Route
+            path="/index"
+            render={ (props) =>
+              <ApartmentIndex
+                logged_in={ logged_in }
+                sign_in_route={ sign_in_route }
+                sign_out_route={ sign_out_route }
+                apartments={ this.state.apartments }
+              />
+            }
+          />
+        </Switch>
+        <Footer
+          logged_in={ this.props.logged_in }
+          sign_in_route={ this.props.sign_in_route }
+          sign_up_route={ this.props.sign_up_route }
+          sign_out_route={ this.props.sign_out_route }
+        />
+      </Router>
     );
   }
 }
